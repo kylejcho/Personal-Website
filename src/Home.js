f4ffd676d6d814b16a1c47c469c38059c04bd5e2
kylejcho <kylejcho@gmail.com>
2021-02-09 21:48:19 -0500

    Setup

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Hello my name is Kyle</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1>KYLE J CHO</h1>
+    </nav>
+  );
+};
+
+export default NavBar;
