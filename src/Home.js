53d0d5eab345c5039ee4585e6eecb4dbb095e195
kylejcho <kylejcho@gmail.com>
2021-02-10 21:43:59 -0500

    home description

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,12 @@ const Home = () => {
     <div className="home">
       <h2 className="homePhrase">Hi, my name is </h2>
       <h2 className="homeName">Kyle</h2>
+      <div className="homeDescription">
+        <p>
+          I’m a front-end web developer from Bethesda, MD. Come see what kind of
+          stuff I’ve been working on!{" "}
+        </p>
+      </div>
     </div>
   );
 };
