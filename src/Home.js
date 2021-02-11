7403a5806b2f0ee7e375fef3226c71c3aa733319
kylejcho <kylejcho@gmail.com>
2021-02-10 21:39:16 -0500

    Home title

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>Hi, my name is </h2>
+      <h2 className="homePhrase">Hi, my name is </h2>
+      <h2 className="homeName">Kyle</h2>
     </div>
   );
 };
